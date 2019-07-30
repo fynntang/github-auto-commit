c2f7744126de546fd7208fd579b9e4b7a78a0d66
weijiyang <weijiyang@meicai.cn>
2019-07-30 15:42:03 +0800

    github - auto - commit

--- auto-commit.js
+++ auto-commit.js
@@ -3,32 +3,38 @@ const app = new koa()
 const schedule = require('node-schedule')
 const chalk = require('chalk')
 const fs = require('fs')
-const promisify = require('util').promisify
 const readFile = fs.readFileSync;
 const writeFile = fs.writeFileSync;
 
-const execPromise = promisify(require('child_process').exec)
+const exec = require('child_process').execSync
 const cmd_pull = "git pull origin master"
 const cmd_add = "git add ."
 const cmd_commit = "git commit -m 'github - auto - commit'"
 const cmd_push = "git push origin master"
 
-async function pullNewCode () {
-    await execPromise(cmd_pull).then(res => {
-        console.log(chalk.green(cmd_pull, ':  ', res))
-    }).catch(err => {
-        console.log(chalk.red(cmd_pull, ':  ', err))
-    })
+try {
+    function pullNewCode () {
+        exec(cmd_pull)
+    }
+    
+    function addNewCode () {
+        let content = readFile('./README.md', 'utf-8')
+        writeFile('./README.md', `${content}\n* ${new Date()}`)
+    }
+    function pushNewCode () {
+        exec(cmd_add)
+        exec(cmd_commit)
+        exec(cmd_push)
+    }
+    let t = schedule.scheduleJob('1 * * * * *',()=>{
+        pullNewCode()
+        addNewCode()
+        console.log('scheduleCronstyle:' + new Date());
+        pushNewCode()
+    });
+} catch (err) {
+    console.log(chalk.red('脚本出错 错误详情： ', err))
 }
 
-function addNewCode () {
-    let content = readFile('./README.md', 'utf-8')
-    writeFile('./README.md', `${content}\n* ${new Date()}`)
-}
-let t = schedule.scheduleJob('* * * * * *',()=>{
-    // pullNewCode()
-    addNewCode()
-    console.log('scheduleCronstyle:' + new Date());
-});
 console.log('启动成功 localhost: 3000')
 app.listen(3000)
